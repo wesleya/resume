b99810cb05ab0db9fdc8a033e723f895a6c8a708
Wesley Agena <wesley.agena@live.com>
2016-07-29 13:27:21 -0700

    updating to latest beta of bootstrap 4

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,7 @@
         <h1 class="display-3 text-xs-center">Contact</h1>
 
         <div class="row section-row">
-            <div class="col-xs-8 col-xs-offset-2 col-sm-4 col-sm-offset-4 col-md-2 col-md-offset-5">
+            <div class="col-xs-8 offset-xs-2 col-sm-4 offset-sm-4 col-md-2 offset-md-5">
                 <img src="/img/profile-pic.png"
                      class="img-rounded center-block profile-pic"
                      alt="profile picture"
@@ -26,11 +26,11 @@
         </div>
 
         <div class="row section-row">
-            <div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
+            <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
                 <div class="row">
                     <div class="col-xs-3">
                         <a href="https://github.com/wesleya"
-                           type="button"
+                           role="button"
                            class="btn btn-default btn-square btn-xl brown"
                         >
                             <i class="fa fa-github-square"></i>
@@ -38,7 +38,7 @@
                     </div>
                     <div class="col-xs-3">
                         <a href="https://www.linkedin.com/in/wesley-agena-39919330"
-                           type="button"
+                           role="button"
                            class="btn btn-default btn-square btn-xl success"
                         >
                             <i class="fa fa-linkedin-square"></i>
@@ -46,7 +46,7 @@
                     </div>
                     <div class="col-xs-3">
                         <a href="https://www.facebook.com/wesley.agena"
-                           type="button"
+                           role="button"
                            class="btn btn-default btn-square btn-xl warning"
                         >
                             <i class="fa fa-facebook-square"></i>
@@ -54,7 +54,7 @@
                     </div>
                     <div class="col-xs-3">
                         <a href="https://twitter.com/wesley837"
-                           type="button"
+                           role="button"
                            class="btn btn-default btn-square btn-xl danger"
                         >
                             <i class="fa fa-twitter-square"></i>
