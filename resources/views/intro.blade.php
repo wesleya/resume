48a7cd01c302580f2c7252edeb206c95f7f2a4be
Wesley Agena <wesley.agena@live.com>
2016-08-03 13:54:38 -0700

    fix font size on different screen sizes

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -1,15 +1,14 @@
-<div class="jumbotron jumbotron-fluid grad" id="intro" style="margin-bottom: 0px;">
+<div class="jumbotron jumbotron-fluid grad" id="intro" style="margin-bottom: 0px; padding: 0px;">
     <div class="container">
         <div class="jumbotron" style="background-color: transparent; margin: 0px;">
-            <h1 style="margin-bottom: 0px;">Hi, I'm</h1>
-            <h1>Wesley Agena</h1>
+            <h1>Hi, I'm Wesley Agena</h1>
 
             <h2 style="padding-top: 20px;">Senior Backend PHP Developer</h2>
         </div>
 
-        <div class="jumbotron" style="background-color: transparent; padding: 32px;">
+        <div class="jumbotron component-transparent" style="padding-top: 0px;">
             <div class="row">
-                <div class="col-xs-12 col-sm-4 col-md-3 btn-container">
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 btn-container">
                     <a class="btn btn-primary btn-lg btn-block"
                        href="#projects"
                        role="button"
@@ -17,7 +16,7 @@
                         Side Projects
                     </a>
                 </div>
-                <div class="col-xs-12 col-sm-4 col-md-3 btn-container">
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 btn-container">
                     <a class="btn btn-primary btn-lg btn-block"
                        href="#contact"
                        role="button"
@@ -25,7 +24,7 @@
                         Contact
                     </a>
                 </div>
-                <div class="col-xs-12 col-sm-4 col-md-3 btn-container">
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 btn-container">
                     <a class="btn btn-primary btn-lg btn-block"
                        href="resume/wesley-agena-resume.pdf"
                        role="button"
